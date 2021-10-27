c110e15da4773ad90753f94e956b4966c4482399
Gaurav Bhattarai <g.bhattarai2005@gmail.com>
2021-10-26 21:17:54 -0400

    fixing dwmblocks

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
+	{"Disk: ", 	"df -h / | awk 'FNR==2 {print $3 \" / \" $4}'",			3600, 			0},
+
+	{"Mem: ", 	"free -h | awk '/^Mem/ { print $3\" / \"$2 }' | sed s/i//g",	30,			0},
+
+	{"", 		"date '+%b %d (%a) %I:%M%p'",					5,			0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
